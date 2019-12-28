c9b92e31507a7888ceaeb88647ad5f65ff09b4f4
marzieh alikhani <alikhani@Faraz.com>
2019-12-28 11:58:41 +0330

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'firstweb';
+  imgurl='https://picsum.photos/200';
+  count=0;
+  name:string;
+  userName:string;
+private _customerName:string;
+  //increament button
+  increamentCount(){
+this.count +=1;
+  }
+
+
+//check person method
+  checkperson(updatedValue){
+
+this.userName=updatedValue;
+if(updatedValue=='marzieh')
+{alert('welcome back marzieh')}
+  
+}
 }
